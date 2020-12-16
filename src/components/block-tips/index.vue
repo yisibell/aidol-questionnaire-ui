4388d154d039d9a23e920d7e13ae9f498b06c733
elenh <yisiwings@163.com>
2020-12-16 11:46:25 +0800

    add: ai-block-tips

---
+++ src/components/block-tips/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="ai-block-tips">
+    <div class="ai-block-tips__content">
+      <slot name="default"></slot>
+    </div>
+    <div class="ai-block-tips__message">
+      <slot name="content">{{ content }}</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AiBlockTips',
+  props: {
+    // success, danger, warning
+    type: {
+      type: String,
+      default: ''
+    },
+    message: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,12 @@
 import AiChoice from './choice'
 import AiTitle from './title'
 import AiShortAnswer from './short-answer'
+import AiBlockTips from './block-tips'
 
 import '@/styles/index.scss'
 
 function install(Vue) {
-  ;[AiChoice, AiTitle, AiShortAnswer].forEach(v => {
+  ;[AiChoice, AiTitle, AiShortAnswer, AiBlockTips].forEach(v => {
     Vue.component(v.name, v)
   })
 }
